ce28ee8f83e1edfc41a8b2e16f14659672c61197
Marcin Dolega <marcin_dolega@epam.com>
2022-05-26 19:45:14 +0200

    "Innitial commit"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Kido!");
-        System.out.println("Hello Tiga!");
+
+        System.out.println(reverse("Kido"));
+
+    }
+
+    static String reverse(String string) {
+        return String.valueOf(new StringBuilder(string).reverse());
     }
 }
